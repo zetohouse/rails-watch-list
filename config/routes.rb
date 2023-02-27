5bbb9a46f6ca16cf8df5585f0b4d71fa7bd00ce7
Jose Wollner <zetowollner@gmail.com>
2023-02-27 14:52:53 -0300

    First part of Watch list - 27/02

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :bookmarks, only: [:new, :create, :destroy] do
+    resources :lists, only: [:index, :show, :new, :create] do
+      resources :movies
+  end
+end
   # Defines the root path route ("/")
   # root "articles#index"
 end
