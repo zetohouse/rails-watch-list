b3ba941ac90f780ea3a3a28da63a00a50b7fdb8e
Jose Wollner <zetowollner@gmail.com>
2023-02-28 15:48:04 -0300

    Rails watch-list

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,41 @@
+class BookmarksController < ApplicationController
+
+  before_action :find_list, only: [:new, :create]
+  before_action :find_bookmark, only: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to @list, notice: "Bookmark was succesfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), notice: "Bookmark was succesfully deleted.", status: :see_other
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :bookmarks, only: [:new, :create, :destroy] do
-    resources :lists, only: [:index, :show, :new, :create] do
-      resources :movies
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Defines the root path route ("/")
+# root "articles#index"
